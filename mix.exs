a8998f2d81c86d3d3d0cab62604acc0ba0eb24f8
Joshua <zenten@zenten.ca>
2017-03-29 00:05:39 -0400

    Now on page 205 of Programming Elixir 1.3

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Issues.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,9 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.9"},
+      {:poison, "~> 2.2"}
+    ]
   end
 end
